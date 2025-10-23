e40d8b46d57b9ea4e01247a6a886c209e62987f0
toa takeuchi <toa0928koron@icloud.com>
2025-10-23 12:45:40 +0900

    Initial Commit

---
+++ OfflineVideoEditor/OfflineVideoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfflineVideoEditorApp.swift
+//  OfflineVideoEditor
+//
+//  Created by 竹内音碧 on 2025/10/23.
+//
+
+import SwiftUI
+
+@main
+struct OfflineVideoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
